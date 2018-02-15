b32101719b9f436b41c0d465a3beb633311aee5b
chihchunhsu <chh194@ucsd.edu>
2018-02-15 01:07:15 -0800

    modified:   utils/listTarget.py

--- utils/listTarget.py
+++ utils/listTarget.py
@@ -14,12 +14,17 @@ def makeTargetList(path, **kwargs):
 	path      : str
 	            The path of the data.
 
+	Options
+	-------
 	outdir    : str
 	            The path where the csv file will be stored.
 	            The default is the same is the path of the data.
 
 	save      : boolean value
-			    True returns a csv file.
+			    True returns a csv file. The default is True.
+
+	name 	  : str
+				name of the csv file. The default is 'targetlist.csv'
 
 
 	Returns
@@ -44,6 +49,7 @@ def makeTargetList(path, **kwargs):
 	cwd    = os.getcwd()
 	outdir = kwargs.get('outdir',path)
 	save   = kwargs.get('save', True)
+	name   = kwargs.get('name', 'targetlist')
 
 	# crear empty lists to store keywords
 	OBJECT_list   = []
@@ -65,7 +71,11 @@ def makeTargetList(path, **kwargs):
 				try:
 					with fits.open(fullpath, ignore_missing_end=True) as f:
 						if f[0].header['IMAGETYP'] == 'object':
-							if f[0].header['OBJECT'] is not null:
+							if f[0].header['OBJECT'] is not null and 'dark' not in f[0].header['OBJECT'] and\
+							'arc' not in f[0].header['OBJECT'] and 'flat' not in f[0].header['OBJECT'] and\
+							'test' not in f[0].header['OBJECT'] and 'Dark' not in f[0].header['OBJECT'] and\
+							'Arc' not in f[0].header['OBJECT'] and 'Flat' not in f[0].header['OBJECT'] and\
+							'Test' not in f[0].header['OBJECT']:
 								OBJECT_list.append(f[0].header['OBJECT'])
 								FILENAME_list.append(f[0].header['FILENAME'])
 								SLITNAME_list.append(f[0].header['SLITNAME'])
@@ -90,9 +100,9 @@ def makeTargetList(path, **kwargs):
 
 	df = pd.DataFrame({"OBJECT" : OBJECT_list, "FILENAME" : FILENAME_list, "SLITNAME":SLITNAME_list, "DATE-OBS":DATEOBS_list, "PROGPI":PROGPI_list})
 	
-	
+	# save as a csv file
 	if save == True:
-		save_to_path = outdir + 'targetlist.csv'
+		save_to_path = outdir + name + '.csv'
 		df.to_csv(save_to_path, index=False)
 		print("The target list is saved to {} .".format(save_to_path))
 
