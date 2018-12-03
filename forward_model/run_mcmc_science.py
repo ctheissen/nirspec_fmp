5b17aa9de641f253a33edf764f15b3b87a695f9c
chihchunhsu <chh194@ucsd.edu>
2018-12-03 02:33:10 -0800

    matplotlib setting

--- forward_model/run_mcmc_science.py
+++ forward_model/run_mcmc_science.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from astropy.io import fits
--- forward_model/run_mcmc_telluric.py
+++ forward_model/run_mcmc_telluric.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import matplotlib
+matplotlib.use('Agg')
 from astropy.io import fits
 import emcee
 #from schwimmbad import MPIPool
