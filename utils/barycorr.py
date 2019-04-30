ec79441d75c2da7703e2a42afc70f61a0d86819b
chihchunhsu <chh194@ucsd.edu>
2019-04-30 00:41:46 -0700

    modify barycorr for apogee

--- utils/barycorr.py
+++ utils/barycorr.py
@@ -18,11 +18,7 @@ import astropy.units as u
 # Need to convert the longitude to the definition in pyasl.helcorr
 # obs_long: Longitude of observatory (degrees, **eastern** direction is positive)
 
-longitude = 360 - (155 + 28.7/60 ) # degrees
-latitude  =  19 + 49.7/60 #degrees
-altitude  = 4160.
 
-keck = EarthLocation.from_geodetic(lat=latitude*u.deg, lon=longitude*u.deg, height=altitude*u.m)
 #`~astropy.coordinates.Longitude` or float
 #   Earth East longitude.  Can be anything that initialises an
 #   `~astropy.coordinates.Angle` object (if float, in degrees)
@@ -39,6 +35,12 @@ def barycorr(header, instrument='nirspec'):
 
 	"""
 	if instrument == 'nirspec':
+		longitude = 360 - (155 + 28.7/60 ) # degrees
+		latitude  =  19 + 49.7/60 #degrees
+		altitude  = 4160.
+
+		keck = EarthLocation.from_geodetic(lat=latitude*u.deg, lon=longitude*u.deg, height=altitude*u.m)
+
 		date    = Time(header['DATE-OBS'], scale='utc')
 		jd      = date.jd
 
@@ -56,12 +58,18 @@ def barycorr(header, instrument='nirspec'):
 			barycorr = sc.radial_velocity_correction(obstime=Time(ut, scale='utc'), location=keck)
 
 	elif instrument == 'apogee':
+		longitude = 360 - (105 + 49.2/60 ) # degrees
+		latitude  =  32 + 46.8/60 #degrees
+		altitude  = 2798.
+
+		apogee = EarthLocation.from_geodetic(lat=latitude*u.deg, lon=longitude*u.deg, height=altitude*u.m)
+
 		ut      = header['UT-MID']
 		ra      = header['RA']
 		dec     = header['DEC']
 		sc      = SkyCoord(ra=ra*u.deg, dec=dec*u.deg, equinox='J2000', frame='fk5')
 
-		apogee  = EarthLocation.of_site('Apache Point Observatory')
+		#apogee  = EarthLocation.of_site('Apache Point Observatory')
 
 		barycorr = sc.radial_velocity_correction(obstime=Time(ut, scale='utc'), location=apogee)
 
